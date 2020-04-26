6d66bcd93cc7c7a9414904b3e89e9f334f4ff2b8
fedecame <fede.cameriere@gmail.com>
2020-04-26 01:39:14 -0300

    Comentarios a modo de guia en Mutacion

--- src/main/java/ar/edu/unq/eperdemic/modelo/Mutacion.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Mutacion.kt
@@ -1,4 +1,9 @@
 package ar.edu.unq.eperdemic.modelo
 
 class Mutacion(var id: Int?) {
+
+    //Hay q agregar:
+    // *cantidad de ADN necesario para mutar
+    // *mutaciones previas necesarias para mutar
+    // *incremento al valor especifico de la especie
 }
