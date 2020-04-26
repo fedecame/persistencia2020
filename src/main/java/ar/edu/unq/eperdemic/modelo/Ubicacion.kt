03dec84e7672357459f490f40ea5b7bc42ef04ba
legajo8716 <50986215+legajo8716@users.noreply.github.com>
2020-04-26 12:32:53 -0300

    agrego fun a ubicacion

--- src/main/java/ar/edu/unq/eperdemic/modelo/Ubicacion.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Ubicacion.kt
@@ -1,4 +1,15 @@
 package ar.edu.unq.eperdemic.modelo
 
 class Ubicacion(var nombreUbicacion: String) {
+
+    private val vectorAlojados = mutableListOf<Vector>()
+
+    fun getVectorAlojados() : List<Vector>{
+
+        return vectorAlojados
+    }
+    fun addVectorUbicacion(vector:Vector){
+        vectorAlojados.add(vector)
+    }
+
 }
