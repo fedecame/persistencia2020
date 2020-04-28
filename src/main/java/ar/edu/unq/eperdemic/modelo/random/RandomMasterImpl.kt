cfa36fd8b6875834e4f248abe5ea96547cef5d8b
ValenzuelaH <valenzuelahoracioe@gmail.com>
2020-04-27 22:11:03 -0300

    Se agrega modulo de Random testeado y con su respectiva excepcion

---
+++ src/main/java/ar/edu/unq/eperdemic/modelo/random/RandomMasterImpl.kt
@@ -0,0 +1,17 @@
+package ar.edu.unq.eperdemic.modelo.random
+
+import ar.edu.unq.eperdemic.modelo.exception.MalosParametrosRunTimeException
+import kotlin.random.Random
+
+
+class RandomMasterImpl : RandomMaster {
+    override fun giveMeARandonNumberBeetween(aNumber : Double, otherNumber : Double) : Double{
+        try {
+            return Random.nextDouble(aNumber, otherNumber)
+        }
+        catch(e : IllegalArgumentException) {
+            throw MalosParametrosRunTimeException(aNumber, otherNumber)
+        }
+    }
+}
+
