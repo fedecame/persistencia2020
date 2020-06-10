932ad82b10b9b30fc71fa02020a8671284bb128a
underabloodysky <valenzuelahoracioe@gmail.com>
2020-06-10 19:40:52 -0300

    Refactor del TransactionRunner para agregar Transactioners segun convenga

--- src/main/java/ar/edu/unq/eperdemic/services/runner/TransactionRunner.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/runner/TransactionRunner.kt
@@ -1,8 +1,51 @@
 package ar.edu.unq.eperdemic.services.runner
 
 
-import org.hibernate.Session
+object TransactionRunner{
+    var transactions: MutableList<Transaction> = mutableListOf(TransactionHibernate())
 
+    private fun addIf(transaction: Transaction): TransactionRunner {
+        if (!this.isThere(transaction)) {
+            transactions.add(transaction)
+        }
+        return this
+    }
+
+    private fun isThere(transaction: Transaction): Boolean = transactions.any { it.javaClass.name == transaction.javaClass.name }
+    private fun forAll(bloque: (Transaction) -> Unit) {
+        transactions.forEach(bloque)
+    }
+    private fun start() {
+        this.forAll { it.start() }
+    }
+    private fun commit() {
+        this.forAll { it.commit() }
+    }
+    private fun rollback() {
+        this.forAll { it.rollback() }
+    }
+    fun clear() {
+        transactions = mutableListOf()
+    }
+
+    fun addHibernate(): TransactionRunner = this.addIf(TransactionHibernate())
+
+    //fun addNeo4j() : TransactionRunner = this.addIf(TransactionNeo4j())
+
+    fun <T> runTrx(bloque: () -> T): T {
+        try {
+            this.start()
+            val resultado = bloque()
+            this.commit()
+            return resultado
+        } catch (e: RuntimeException) {
+            this.rollback()
+            throw(e)
+        }
+    }
+}
+
+     /*
 object TransactionRunner {
     private var transactions:List<Transaction> = listOf(TransactionHibernate())
 
@@ -18,7 +61,6 @@ object TransactionRunner {
         }
     }
 
-     /*
     fun <T> runTrx(bloque: ()->T): T {
         session = SessionFactoryProvider.instance.createSession()
         session.use {
@@ -37,4 +79,3 @@ object TransactionRunner {
     }
     */
 
-}
