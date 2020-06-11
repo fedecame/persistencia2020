e4947fe513e34da8416f70e9884232cb25c252df
Federico Cameriere <fede.cameriere@gmail.com>
2020-06-11 06:44:09 -0300

    emprolijado minimo

--- src/main/java/ar/edu/unq/eperdemic/services/runner/TransactionRunner.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/runner/TransactionRunner.kt
@@ -1,6 +1,5 @@
 package ar.edu.unq.eperdemic.services.runner
 
-
 object TransactionRunner{
     var transactions: MutableList<Transaction> = mutableListOf(TransactionHibernate, TransactionNeo4j)
 
@@ -44,38 +43,3 @@ object TransactionRunner{
         }
     }
 }
-
-     /*
-object TransactionRunner {
-    private var transactions:List<Transaction> = listOf(TransactionHibernate())
-
-    fun <T> runTrx(bloque: ()->T): T {
-        try{
-           transactions.forEach { it.start() }
-           val result = bloque()
-           transactions.forEach { it.commit() }
-           return result
-        } catch (exception:Throwable){
-           transactions.forEach { it.rollback() }
-           throw exception
-        }
-    }
-
-    fun <T> runTrx(bloque: ()->T): T {
-        session = SessionFactoryProvider.instance.createSession()
-        session.use {
-            val tx =  session!!.beginTransaction()
-            try {
-                //codigo de negocio
-                val resultado = bloque()
-                tx!!.commit()
-                return resultado
-            } catch (e: RuntimeException) {
-                tx.rollback()
-                throw e
-            }
-        }
-        session = null
-    }
-    */
-
