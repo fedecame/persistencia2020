91fecac47634181ba24e691e5034508deb5e5eff
underabloodysky <valenzuelahoracioe@gmail.com>
2020-06-21 08:35:14 -0300

    Test en verde pero dummy

--- src/test/java/ar/edu/unq/eperdemic/persistencia/dao/jdbc/FeedDAOTest.kt
+++ src/test/java/ar/edu/unq/eperdemic/persistencia/dao/jdbc/FeedDAOTest.kt
@@ -2,18 +2,19 @@ package ar.edu.unq.eperdemic.persistencia.dao.jdbc
 
 import ar.edu.unq.eperdemic.modelo.Patogeno
 import ar.edu.unq.eperdemic.modelo.Vector
-import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateEspecieDAO
-import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernatePatogenoDAO
-import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateUbicacionDAO
-import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateVectorDAO
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.*
 import ar.edu.unq.eperdemic.persistencia.dao.mongoDB.FeedMongoDAO
+import ar.edu.unq.eperdemic.persistencia.dao.neo4j.Neo4jDataDAO
+import ar.edu.unq.eperdemic.services.HibernateDataService
 import ar.edu.unq.eperdemic.services.PatogenoService
 import ar.edu.unq.eperdemic.services.UbicacionService
 import ar.edu.unq.eperdemic.services.VectorService
 import ar.edu.unq.eperdemic.services.impl.PatogenoServiceImpl
 import ar.edu.unq.eperdemic.services.impl.UbicacionServiceImpl
 import ar.edu.unq.eperdemic.services.impl.VectorServiceImpl
+import ar.edu.unq.eperdemic.services.runner.TransactionRunner
 import ar.edu.unq.eperdemic.tipo.Humano
+import ar.edu.unq.eperdemic.utils.DataService
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -24,14 +25,15 @@ class FeedDAOTest {
     lateinit var ubicacionService : UbicacionService
     lateinit var patogenoService : PatogenoService
     lateinit var vectorService : VectorService
-
-    @Before
+    lateinit var hibernateData : DataService
+            @Before
     fun setUp(){
         dao = FeedMongoDAO()
         val ubicacionDAO = HibernateUbicacionDAO()
         ubicacionService = UbicacionServiceImpl(ubicacionDAO)
         patogenoService = PatogenoServiceImpl(HibernatePatogenoDAO(), HibernateEspecieDAO())
         vectorService = VectorServiceImpl(HibernateVectorDAO(), ubicacionDAO)
+        hibernateData = HibernateDataService()
     }
 
     @Test
@@ -42,13 +44,13 @@ class FeedDAOTest {
 
     @Test
     fun alBuscarLosEventosDContagioPorConvertirseEnPandemiaDerUnTipoDePatogenoQueSiEsPandemiaRetornaUnaListaCon1Evento(){
-        //Una especie se vuelve pandemia se encuentra presenta en la mitad de las locaciones
+        //Una especie se vuelve pandemia se encuentra presenta en mas de la mitad de las locaciones
         val jamaica = ubicacionService.crearUbicacion("Jamaica")
         val babilonia = ubicacionService.crearUbicacion("Babilonia")
+        ubicacionService.crearUbicacion("NismanLandia")
         val patogenoModel = Patogeno()
         patogenoModel.tipo = "virus"
         val especie = patogenoService.agregarEspecie(patogenoService.crearPatogeno(patogenoModel), "gripe", "Narnia")
-
         val vectorJamaiquino = Vector()
         vectorJamaiquino.ubicacion = jamaica
         vectorJamaiquino.tipo = Humano()
@@ -64,9 +66,12 @@ class FeedDAOTest {
         //Assert.assertEquals(1, result.size)
     }
 
-
     @After
     fun dropAll() {
         dao.deleteAll()
+        TransactionRunner.addNeo4j().addHibernate().runTrx {
+            HibernateDataDAO().clear()
+            Neo4jDataDAO().clear()
+        }
     }
 }
