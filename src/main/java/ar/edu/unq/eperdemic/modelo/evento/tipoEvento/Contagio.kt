b80eb2602aa41c915e3716ad1ac12084cae7aa3d
underabloodysky <valenzuelahoracioe@gmail.com>
2020-06-22 07:48:33 -0300

    Se agrega query a medio camino

--- src/main/java/ar/edu/unq/eperdemic/modelo/evento/tipoEvento/Contagio.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/evento/tipoEvento/Contagio.kt
@@ -1,8 +1,5 @@
 package ar.edu.unq.eperdemic.modelo.evento.tipoEvento
 
-import ar.edu.unq.eperdemic.modelo.evento.Evento
-import org.bson.codecs.pojo.annotations.BsonProperty
-
 class Contagio : TipoEvento(){
     override fun log(): String = ""
 }
--- src/main/java/ar/edu/unq/eperdemic/persistencia/dao/mongoDB/FeedMongoDAO.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/dao/mongoDB/FeedMongoDAO.kt
@@ -1,8 +1,13 @@
 package ar.edu.unq.eperdemic.persistencia.dao.mongoDB
 
+import ar.edu.unq.eperdemic.modelo.evento.Accion
 import ar.edu.unq.eperdemic.modelo.evento.Evento
 import ar.edu.unq.eperdemic.modelo.evento.tipoEvento.TipoEvento
 import ar.edu.unq.eperdemic.persistencia.dao.FeedDAO
+import com.mongodb.client.model.Aggregates
+import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Filters.*
+import com.mongodb.client.model.Indexes
 
 class FeedMongoDAO : GenericMongoDAO<Evento>(Evento::class.java), FeedDAO {
 
@@ -11,7 +16,16 @@ class FeedMongoDAO : GenericMongoDAO<Evento>(Evento::class.java), FeedDAO {
     fun getByTipoEvento(tipoEvento: TipoEvento): List<Evento?> = findEq("tipoEvento", tipoEvento)
 
     override fun feedPatogeno(tipoPatogeno : String) : List<Evento>{
-        return listOf()
+        //Me fijo que: Dado un evento, ese evento
+        //      ((Sea de Por Pandemia **O** Por Contagio por Primera vez) **Y** (Sea del tipo de patogeno dado))
+        val match = Aggregates.match(
+                and
+                (or (eq("eventos.accion", Accion.PATOGENO_ES_PANDEMIA.name), eq("eventos.accion", Accion.PATOGENO_CONTAGIA_1RA_VEZ_EN_UBICACION.name)),
+                     eq("eventos.tipoPatogeno", tipoPatogeno))
+        )
+//      val sort = Aggregates.sort(Indexes.descending("fecha"))
+        return aggregate(listOf(match/*, sort*/), Evento::class.java)
+//      return listOf()
     }
 
     override fun feedVector(tipoPatogeno: String): List<Evento> {
