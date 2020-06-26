0de8391b684df17275ceb394d39182735467b0d0
matotec <matias.chaile.1988@gmail.com>
2020-06-26 04:02:10 -0300

    se agrega el mensaje en el factory para el evento crear especie

--- src/main/java/ar/edu/unq/eperdemic/modelo/evento/tipoEvento/Mutacion.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/evento/tipoEvento/Mutacion.kt
@@ -7,7 +7,7 @@ class Mutacion : TipoEvento() {
 
     override fun log(evento: Evento) : String {
         return when (evento.accionQueLoDesencadena) {
-            Accion.ESPECIE_CREADA.name -> "Se crea la especie ${evento.nombreEspecie} "
+            Accion.ESPECIE_CREADA.name -> "Se crea la especie ${evento.nombreEspecie} del patogeno ${evento.tipoPatogeno} "
             Accion.ESPECIE_MUTADA.name -> "La especie ${evento.nombreEspecie} del patogeno ${evento.tipoPatogeno} ha mutado"
             else -> ""
         }
